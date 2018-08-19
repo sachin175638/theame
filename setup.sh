8604f98c4c52637f4ff7d39cd08af1bb2eb97539
sachin175638 <35227965+sachin175638@users.noreply.github.com>
2018-08-19 15:34:51 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+if [ -d $HOME/theame ]
+then
+	echo ""
+else
+	echo "theame folder in not in HOME directory "
+	echo "Moving theame folder to home directry"
+	echo ""
+	mv `pwd` $HOME
+	cd $HOME/theame
+	sh setup.sh
+	exit
+
+fi 
+apt-get update
+apt-get upgrade
+if [ -e /data/data/com.termux/files/usr/bin/python2.7 ]
+then
+	echo ""
+	echo "Python2 already instslled"
+	echo ""
+else
+	apt-get install python2
+fi 
+chmod +x *
+cd modules
+chmod +x *
+
